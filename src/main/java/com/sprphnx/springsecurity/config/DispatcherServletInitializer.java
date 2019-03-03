b1a80a9fefb9d185bd2645fcb98a2de01cfe39e3
rohanpa <rohanpa@aecl.com>
2019-03-03 10:34:05 +0300

    Initial commit

---
+++ src/main/java/com/sprphnx/springsecurity/config/DispatcherServletInitializer.java
@@ -0,0 +1,24 @@
+package com.sprphnx.springsecurity.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {AppConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[]{"/"};
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/view/homePage.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to Spring Security Demo</title>
+</head>
+<body>
+<h1>Hey, No WEB.XML !!!!</h1>
+</body>
+</html>
